862692c1f594dc854cfcf2995ada9b137b57f65b
sxwebdev <sxwebdev@gmail.com>
2025-12-06 23:14:51 +0300

    feat: add map handling in field struct and enhance Set method for map (#3)

--- plugins/defaults/defaults_test.go
+++ plugins/defaults/defaults_test.go
@@ -9,11 +9,31 @@ import (
 	"github.com/sxwebdev/xconfig/plugins/defaults"
 )
 
+type mapKeyType string
+
+const (
+	mapKey1 mapKeyType = "key1"
+)
+
+type nestedStruct struct {
+	InnerField string `default:"inner-default"`
+	BoolVal    bool   `default:"true"`
+	IntVal     int    `default:"7"`
+}
+
+type mapValue struct {
+	Name    string `default:"name-default"`
+	Value   int    `default:"42"`
+	BoolVal bool   `default:"true"`
+	Nested  nestedStruct
+}
+
 type fDefaults struct {
 	Address string        `default:"https://blah.bleh"`
 	Bases   []string      `default:"list,blah"`
 	Timeout time.Duration `default:"5s"`
 	Ignored string
+	Map     map[mapKeyType]mapValue
 }
 
 func TestDefaultTag(t *testing.T) {
@@ -22,9 +42,26 @@ func TestDefaultTag(t *testing.T) {
 		Bases:   []string{"list", "blah"},
 		Timeout: 5 * time.Second,
 		Ignored: "not-empty",
+		Map: map[mapKeyType]mapValue{
+			mapKey1: {
+				Name:    "name-default",
+				Value:   42,
+				BoolVal: true,
+				Nested: nestedStruct{
+					InnerField: "inner-default",
+					BoolVal:    true,
+					IntVal:     7,
+				},
+			},
+		},
 	}
 
-	value := fDefaults{Ignored: "not-empty"}
+	value := fDefaults{
+		Ignored: "not-empty",
+		Map: map[mapKeyType]mapValue{
+			mapKey1: {},
+		},
+	}
 
 	conf, err := xconfig.Custom(&value, defaults.New())
 	if err != nil {
